98dda1c1101bb4dca284a43e37c4fedd6c69f8ac
koioats <raccoon@tuta.com>
2024-09-05 19:55:53 -0700

    command line input

--- cbzdownscaler.cpp
+++ cbzdownscaler.cpp
@@ -5,18 +5,20 @@
 #include <fstream>
 #include <cstdio>
 
-int main() {
-    std::string fileName;
-    std::cout << "Enter a file name: ";
-    std::cin >> fileName;
-    std::ofstream cbz(fileName);
-    std::string fileNameShort = fileName;
+int main(int argc, char *argv[]) {
+    std::string filePath = argv[1];
+    std::ofstream cbz(filePath);
+    std::cout << "File: " + filePath + "\n";
+    system("pause");
+
+    // i have no idea what i'm doing ToT
+/*     std::string fileNameShort = fileName;
     fileNameShort.erase(fileNameShort.length() - 4, 4);
     fileNameShort = fileNameShort + ".zip";
     std::cout << "File opened\n";
     std::rename(fileName.c_str(), fileNameShort.c_str());
     cbz.close();
     std::cout << "File closed\n";
-    system("pause");
+    system("pause"); */
     return 0;
 }
